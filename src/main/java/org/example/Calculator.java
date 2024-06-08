ebc79c5d2a4b1a447665fd3c45aa93b0b93b1e74
vandatnguyen919 <vandatnguyen919@gmail.com>
2024-06-08 17:16:54 +0700

    first commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float divide(float a, float b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/TestCalculatorCSV.java
@@ -0,0 +1,37 @@
+import org.example.Calculator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCalculatorCSV {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/add.csv", numLinesToSkip = 1)
+    void test1(int a, int b, int result) {
+        Calculator math = new Calculator();
+        assertEquals(result, math.add(a,b));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/substract.csv", numLinesToSkip = 1)
+    void test2(int a, int b, int result) {
+        Calculator math = new Calculator();
+        assertEquals(result, math.substract(a,b));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/multiply.csv", numLinesToSkip = 1)
+    void test3(int a, int b, int result) {
+        Calculator math = new Calculator();
+        assertEquals(result, math.multiply(a,b));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/divide.csv", numLinesToSkip = 1)
+    void test4(float a, float b, float result) {
+        Calculator math = new Calculator();
+        float actual = math.divide(a, b);
+        float delta = 0.0001f; // Specify a suitable delta for floating-point comparison
+        assertEquals(result, actual, delta);
+    }
+}
